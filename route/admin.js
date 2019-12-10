1fcce0f98f5c25adc3f9a2f6050bee37783e5a2f
ifer-itcast <yangkangkang@itcast.cn>
2019-12-10 15:44:46 +0800

    登录流程之后端处理

--- route/admin.js
+++ route/admin.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const {User} = require('../model/user');
 const admin = express.Router();
 
 admin.get('/login', (req, res) => {
@@ -10,11 +10,29 @@ admin.get('/user', (req, res) => {
     res.render('admin/user', {});
 });
 
-admin.post('/login', (req, res) => {
+admin.post('/login', async (req, res) => {
     const {email, password} = req.body;
     if(email.trim().length === 0 || password.trim().length === 0) {
         res.status(400).render('admin/error', {
             msg: '用户名或密码不能为空'
+        });
+    }
+    const user = await User.findOne({email});
+
+    if(user) {
+        // 证明邮箱存在
+        if(password == user.password) {
+            // 才让登录
+            res.send('登录成功');
+        } else {
+            res.status(400).render('admin/error', {
+                msg: '密码错误'
+            });
+        }
+    } else {
+        // 证明邮箱根本不存在
+        res.status(400).render('admin/error', {
+            msg: '用户不存在'
         });
     }
 });
