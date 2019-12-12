ebd9a3359341d8122e298bc33e0e1b3ca6ad720c
ifer-itcast <yangkangkang@itcast.cn>
2019-12-12 16:38:42 +0800

    用户列表分页

--- route/admin/userPage.js
+++ route/admin/userPage.js
@@ -1,9 +1,24 @@
 const {User} = require('../../model/user');
 
 module.exports = async (req, res) => {
-    const users = await User.find();
+    // 当前页码
+    let page = req.query.page || 1;
+    // 每一页显示多少条数据
+    let pagesize = 2;
+    // 查询数据库中的总的条数
+    let count = await User.countDocuments();
+    // 计算前端需要显示多少页
+    let total = Math.ceil(count / pagesize);
+    // 根据页码需要跳过多少条
+    // 1 => 0
+    // 2 => 2
+    // 3 => 4
+    let start = (page - 1) * pagesize;
+    const users = await User.find().skip(start).limit(pagesize);
     res.render('admin/user', {
         username: req.session.username,
-        users
+        users,
+        total,
+        page
     });
 };
