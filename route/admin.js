a93d913ac6c8f57e382010ee158da8083cb71cb9
ifer-itcast <yangkangkang@itcast.cn>
2019-12-12 10:43:55 +0800

    获取数据

--- route/admin.js
+++ route/admin.js
@@ -6,4 +6,8 @@ admin.get('/user', require('./admin/userPage'));
 admin.post('/login', require('./admin/login'));
 admin.get('/logout', require('./admin/logout'));
 
+// 新增用户
+admin.get('/user-edit', require('./admin/user-edit'));
+admin.post('/user-edit', require('./admin/user-edit-fn'));
+
 module.exports = admin;
