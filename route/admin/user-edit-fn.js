e4ab20c02f40d43a255123dfa20ce56f7cdf733b
ifer-itcast <yangkangkang@itcast.cn>
2019-12-12 11:45:28 +0800

    后端校验新增用户信息

--- route/admin/user-edit-fn.js
+++ route/admin/user-edit-fn.js
@@ -1,3 +1,19 @@
-module.exports = (req, res) => {
-    res.send(req.body);
+const Joi = require('joi');
+
+module.exports = async (req, res) => {
+    let schema = {
+        username: Joi.string().min(2).max(12).required().error(new Error('用户名不符合规则')),
+        email: Joi.string().email().error(new Error('邮箱格式不符合要求')),
+        password: Joi.string().regex(/^[a-zA-Z0-9]{3,30}$/).required().error(new Error('密码格式不符合要求')),
+        role: Joi.string().valid('normal', 'admin').required().error(new Error('角色非法')),
+        state: Joi.number().valid(0, 1).required().error(new Error('状态值非法'))
+    };
+
+    try {
+        let result = await Joi.validate(req.body, schema);
+        console.log(result);
+        res.send('ok')
+    } catch(err) {
+        res.redirect(`/admin/user-edit?message=${err.message}`);
+    }
 };
