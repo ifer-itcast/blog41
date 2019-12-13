29bf359ef1c128d4a57dec5729c554c08dda1b08
ifer-itcast <yangkangkang@itcast.cn>
2019-12-13 09:38:07 +0800

    修改前期准备

--- route/admin/user-edit.js
+++ route/admin/user-edit.js
@@ -1,6 +1,20 @@
-module.exports = (req, res) => {
-    const {message} = req.query;
-    res.render('admin/user-edit', {
-        message
-    });
+const {User} = require('../../model/user');
+
+module.exports = async (req, res) => {
+    const {message,id} = req.query;
+    if(id) {
+        let user = await User.findOne({_id: id});
+        res.render('admin/user-edit', {
+            message,
+            user,
+            buttonTxt: '修改',
+            link: '/admin/user-modify'
+        });
+    } else {
+        res.render('admin/user-edit', {
+            message,
+            buttonTxt: '添加',
+            link: '/admin/user-edit'
+        });
+    }
 };
