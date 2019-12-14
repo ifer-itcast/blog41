520716f70f26e7c2f04cc03ab1d3fa5facef3495
ifer-itcast <yangkangkang@itcast.cn>
2019-12-14 15:06:12 +0800

    config配置网站信息

--- model/connect.js
+++ model/connect.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
+const config = require('config');
 // 192.168.74.62
-mongoose.connect('mongodb://ifer:ifer@localhost/blog41', {
+// 'mongodb://ifer:ifer@localhost/blog41'
+mongoose.connect(`mongodb://${config.get('db.user')}:${config.get('db.pwd')}@${config.get('db.host')}:${config.get('db.port')}/${config.get('db.name')}`, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(() => console.log('~~~~~~~~~~~~~~~~~~~~~~连接数据库成功~~~~~~~~~~~~~~~~~~~~~~')).catch(err => console.log(err, '连接数据库失败'));
