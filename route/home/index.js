18271bbbaf7d13b464e485512e937aa9d5396e26
ifer-itcast <yangkangkang@itcast.cn>
2019-12-14 16:57:31 +0800

    前台文章展示

--- route/home/index.js
+++ route/home/index.js
@@ -1,3 +1,7 @@
-module.exports = (req, res) => {
-    res.render('home/default', {});
+const {Article} = require('../../model/article');
+const pagination = require('mongoose-sex-page');
+
+module.exports = async (req, res) => {
+    let aritcles = await pagination(Article).find().populate('author').page(1).size(2).display(5).exec();
+    res.render('home/default', {aritcles});
 }
