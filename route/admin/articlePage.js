bb38885636ca82588d80bf0f8e90ac052c32e3ce
ifer-itcast <yangkangkang@itcast.cn>
2019-12-14 09:49:32 +0800

    文章列表

--- route/admin/articlePage.js
+++ route/admin/articlePage.js
@@ -1,6 +1,10 @@
-module.exports = (req, res) => {
+const {Article} = require('../../model/article');
+
+module.exports = async (req, res) => {
     req.app.locals.currentLink = 'article';
+    let articles = await Article.find().populate('author');
     res.render('admin/article', {
-        username: req.session.username
+        username: req.session.username,
+        articles
     });
 };
