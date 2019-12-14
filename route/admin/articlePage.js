55a446215905fb80a474c95c51123013d3d97de2
ifer-itcast <yangkangkang@itcast.cn>
2019-12-14 10:47:12 +0800

    文章分页（mongoose-sex-page）

--- route/admin/articlePage.js
+++ route/admin/articlePage.js
@@ -1,8 +1,15 @@
 const {Article} = require('../../model/article');
+const pagination = require('mongoose-sex-page');
 
 module.exports = async (req, res) => {
     req.app.locals.currentLink = 'article';
-    let articles = await Article.find().populate('author');
+    let page = req.query.page || 1;
+    // page => 查询哪一页
+    // size => 每页展示多少条
+    // display => 展示多少页
+    // exec => 执行前面的查询操作
+    let articles = await pagination(Article).find().page(page).size(2).display(3).populate('author').exec();
+    // res.send(articles);
     res.render('admin/article', {
         username: req.session.username,
         articles
