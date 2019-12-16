b825679853835fb709eb5c2bdc02e5d1725a6ff4
ifer-itcast <yangkangkang@itcast.cn>
2019-12-16 09:33:01 +0800

    分页和详情

--- route/home/article.js
+++ route/home/article.js
@@ -1,3 +1,9 @@
-module.exports = (req, res) => {
-    res.render('home/article');
+const {Article} = require('../../model/article');
+
+module.exports = async (req, res) => {
+    const _id = req.query.id;
+
+    let article = await Article.findOne({_id}).populate('author');
+
+    res.render('home/article', {article});
 }
--- route/home/index.js
+++ route/home/index.js
@@ -2,6 +2,8 @@ const {Article} = require('../../model/article');
 const pagination = require('mongoose-sex-page');
 
 module.exports = async (req, res) => {
-    let aritcles = await pagination(Article).find().populate('author').page(1).size(2).display(5).exec();
-    res.render('home/default', {aritcles});
+    let page = req.query.page || 1;
+    let article = await pagination(Article).find().populate('author').page(page).size(2).display(5).exec();
+    // res.send(aritcles);
+    res.render('home/default', {article});
 }
